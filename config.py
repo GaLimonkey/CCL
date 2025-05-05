573a6a0e1518c87152240419c093b861d2d15f15
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2025-05-05 14:47:25 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    UPLOAD_FOLDER = 'app/uploads'
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
